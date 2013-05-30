694140a7c0d99d9d4e7453e81d7b5041d158db34
Alan Elkner <aelkner@gmail.com>
2013-05-30 15:43:20 -0400

    added settings files from cosmotology example

---
+++ settings_dev.py.ex
@@ -0,0 +1,16 @@
+from base_settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '', # Or path to database file if using sqlite3.
+        'USER': '', # Not used with sqlite3.
+        'PASSWORD': '', # Not used with sqlite3.
+        'HOST': '', # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '', # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+SECRET_KEY = < replace with your own secret key, and keep it a secret! >
+
+INSTALLED_APPS = BASE_INSTALLED_APPS
