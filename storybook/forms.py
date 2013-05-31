935f65d1c5b16be3a1456fb39626f06344c3eb5b
Daniel Hilla <danielhilla@gmail.com>
2013-05-31 09:18:53 -0400

    added action field to forms.py; added submit field to writinganewnode.html;

--- storybook/forms.py
+++ storybook/forms.py
@@ -2,4 +2,5 @@ from django import forms
 from tinymce.widgets import TinyMCE
 
 class NodeForm(forms.Form):
-    text = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 15}),max_length=200)
+    action = forms.CharField(min_length=2, max_length=30, label="What do you want to do next?")
+    text = forms.CharField(widget=TinyMCE(attrs={'cols': 80, 'rows': 15}),max_length=200, label="")
