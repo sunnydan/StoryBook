e02562749d972173d62eac3da63512233c2e64df
Daniel Hilla <danielhilla@gmail.com>
2013-06-05 12:07:05 -0400

    Added ability for users to edit their nodes, and staff to edit any node

--- storybook/views.py
+++ storybook/views.py
@@ -21,6 +21,7 @@ def node(request, nodeid):
     node = findNode(nodeid)
     if not node:
         return go404()
+    node_is_users = False
     nextnodes = Node.objects.all().filter(parent=node)
     nextnode1 = None
     nextnode2 = None
@@ -34,11 +35,14 @@ def node(request, nodeid):
     approved_already = False
     if request.user.is_authenticated():
         properties = findProperties(request.user)
+        if node.author == findUser(request.user):
+            node_is_users = True
         if properties.already_approved_nodes.all().filter(id = nodeid):
             approved_already = True
     print(node.points)
     context = {
         'node': node,
+        'node_is_users': node_is_users,
         'nextnode1': nextnode1, 
         'nextnode2': nextnode2, 
         'nextnode3': nextnode3,
@@ -52,6 +56,30 @@ def profile(request):
     else:
         return goHome()
 
+def editnode(request, nodeid):
+    node = findNode(nodeid)
+    if not node:
+        return go404()
+    if request.user.is_staff or node.author == findUser(request.user):
+        already_written = {'action': node.action, 'text': node.text}
+        form = NodeForm(already_written)
+        return render_to_response("editinganode.html", {'form': form, 'node': node}, context_instance=RequestContext(request))
+    return goHome()
+
+def submiteditednode(request, nodeid):
+    if request.user.is_authenticated() and request.method == "POST":
+        if request.user.is_staff or node.author == findUser(request.user):
+            node = findNode(nodeid)
+            if not node:
+                return go404()
+            form = NodeForm(request.POST)
+            if form.is_valid():
+                node.action = form.cleaned_data['action']
+                node.text = form.cleaned_data['text']
+                node.save()
+                return HttpResponseRedirect("/node:"+str(node.id)+"/") 
+    return goHome()
+
 def writenextnode(request, parentid):
     if request.user.is_authenticated() and 'nodeid' in request.GET and request.GET['nodeid'] == parentid:
         if (not parentid and user.is_staff()) or parentid:
@@ -59,7 +87,7 @@ def writenextnode(request, parentid):
             return render_to_response("writinganewnode.html", {'form': form, 'parentid': parentid}, context_instance=RequestContext(request))
     return goHome()
 
-def submitnode(request, parentid):
+def submitnewnode(request, parentid):
     if request.user.is_authenticated() and request.method == "POST":
         if (not parentid and user.is_staff()) or parentid:    
             form = NodeForm(request.POST)
