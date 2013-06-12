755ade42fa966d70e4ddbb183e1620c666e96d8c
Daniel Hilla <danielhilla@gmail.com>
2013-06-12 09:13:14 -0400

    Added three methods to the Page Object to make the view tree program easier

--- storybook/stories/models.py
+++ storybook/stories/models.py
@@ -8,7 +8,23 @@ class Page(models.Model):
     illustration = models.ImageField(upload_to='illustrations/%Y/%m/%d')
     short_desc = models.CharField(max_length=30)
     long_desc = models.TextField()
-
+    
+    def child1(self):
+        if Pages.objects.all().filter(parent=self):
+          return Pages.objects.all().filter(parent=self)[0]
+        return None
+    
+    def child2(self):
+        if len(Pages.objects.all().filter(parent=self)) > 1: 
+          return Pages.objects.all().filter(parent=self)[1]
+        return None
+    
+    def get_root(self):
+        if not parent:
+          return self
+        else:
+          return parent.get_root()
+        
     def __unicode__(self):
         return str(self.short_desc)
 
